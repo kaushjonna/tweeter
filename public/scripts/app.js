2618c18df118d1f5ef0f000ff2e22237c0122456
Kaush <kaush.jonna@gmail.com>
2019-02-21 00:30:50 +0000

    made changes adding some animations and UX features. added validation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,23 +1,51 @@
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-function escape(str) {
+//Helper Functions
+const escape = (str) => {
   var div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
 
-$(document).ready(() => {
+const timeCalc = (uTime) => {
+  const date = new Date();
+  const diffTime = date - uTime;
+  const daysElapsed = Math.round(diffTime / 24 / 60 / 60 / 1000);
+  return daysElapsed;
+}
+
+$(document).ready(() => { //checks for document readyness. everything is loaded up before executing these functions.
+
   const validate = () => {
     const tweet = $('#tweet-input-textarea').val();
     if (tweet === "") {
-      alert('there is no tweet');
+      $('#alerts').css({ 'visibility': 'visible' });
+      $('#alerts').slideDown(() => {
+        $('#alerts').text("Warning: No content to tweet. Please enter something in the box.");
+        setTimeout(() => {
+          $('#alerts').slideUp(() => {
+            console.log('done')
+          });
+        }, 4000);
+        console.log('triggered');
+      });
       return false;
     } else if (tweet.length > 140) {
-      alert('Tweet Too long, please keep under 140 characters');
+      $('#alerts').css({ 'visibility': 'visible' });
+      $('#alerts').slideDown(() => {
+        $('#alerts').text("Warning: tweet longer than 140 characters.");
+        setTimeout(() => {
+          $('#alerts').slideUp(() => {
+            console.log('done')
+          });
+        }, 4000);
+        console.log('triggered');
+      });
       return false;
     }
     return true;
@@ -61,23 +89,23 @@ $(document).ready(() => {
   const createTweetElement = (tweet) => {
     const composed = $('<section id="tweet-container">').append(
       `<article>
-      <header>
-    <img src=${tweet.user.avatars.small}>
-    <h1>${tweet.user.name}</h1>
-    <p>${tweet.user.handle}</p>
-  </header>
+        <header>
+          <img src=${tweet.user.avatars.small}>
+          <h1>${tweet.user.name}</h1>
+          <p>${tweet.user.handle}</p>
+        </header>
 
-  <p id="tweet-text">${escape(tweet.content.text)}</p>
+        <p id="tweet-text">${escape(tweet.content.text)}</p>
 
-  <footer>
-    ${timeCalc(tweet.created_at)} days ago.
-    <span id="action-icons">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="fas fa-heart"></i>
-    </span>
-  </footer>
-  </article>`
+        <footer>
+          ${timeCalc(tweet.created_at)} days ago.
+          <span id="action-icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </span>
+        </footer>
+      </article>`
     );
 
     return composed;
@@ -87,12 +115,13 @@ $(document).ready(() => {
     $('#all-tweets').append(composedTweet);
   }
 
-  const timeCalc = (uTime) => {
-    const date = new Date();
-    const diffTime = date - uTime;
-    const daysElapsed = Math.round(diffTime / 24 / 60 / 60 / 1000);
-    return daysElapsed;
-  }
+  $(() => {
+    $('#compose-tweet').click(() => {
+      $('#tweetForm').slideToggle("slow", () => {
+        $('#tweet-input-textarea').focus();
+      });
+    });
+  });
 
   loadTweets();
 });
